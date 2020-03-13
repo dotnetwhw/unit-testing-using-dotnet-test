01e373b810dc6a6acd4fab5b49e8536be3ca3103
Huawei Wang <871155310@qq.com>
2020-03-13 16:26:16 +0800

    init project

---
+++ PrimeService.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+
+namespace PrimeService.Tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+        
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+    
+            Assert.False(result, userMessage: $"{value} should not be prime");
+        }
+    }
+}
